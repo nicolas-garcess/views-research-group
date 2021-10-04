7eef9d76bcd7647097e7f808535dc2d2ceb8db13
garciaomalejandra <garciaomalejandra@gmail.com>
2021-10-03 20:52:32 -0500

    creation frontend login

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import './index.css';
 
 function Login() {
   return (
     <div className="login">
-      <h1>Login</h1>
+      <div className="container">
+        <img src="/assets/images/coal.png" alt="coal" />
+        <form className="form">
+          <div className="form-upper">
+            <label htmlFor="user">NOMBRE DE USUARIO</label>
+            <input type="text" id="user" placeholder="nombre de usuario" />
+            <label htmlFor="password">CONTRASEÑA</label>
+            <input type="password" id="password" placeholder="contraseña" />
+            <p>¿NO SE ENCUENTRA REGISTRADO?</p>
+            <a href="https://www.google.com/">REGISTRARSE</a>
+          </div>
+          <div className="button">
+            <button type="button">INICIAR SESIÓN</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
